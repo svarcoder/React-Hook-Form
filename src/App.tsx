fde8118a76c4329ed5e8c4400c4ae0ec4ae1a223
“svarcoder” <charvai1997@gmail.com>
2023-09-24 21:09:30 +0530

    Final_Done

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import "./App.css";
+// import { RHFYouTubeForm } from "./components/RHFYouTubeForm";
+// import { YupYouTubeForm } from "./components/YupYouTubeForm";
+// import { ZodYouTubeForm } from "./components/ZodYouTubeForm";
+import { LoginForm } from "./components/MuiLoginForm";
+
+function App() {
+  return (
+    <div>
+      {/* <RHFYouTubeForm /> */}
+      {/* <YupYouTubeForm /> */}
+      {/* <ZodYouTubeForm /> */}
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
